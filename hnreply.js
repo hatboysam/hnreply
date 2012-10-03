d2c547818245e16e83d0b91ff34a49ad36828e8f
Sam Stern <hatboysam@gmail.com>
2012-10-03 01:08:44 -0400

    Code style change, preparing for merge with HNES

--- hnreply.js
+++ hnreply.js
@@ -1,55 +1,60 @@
-var postLoc = "http://news.ycombinator.com/r"
+var InlineReply = {
 
-$('a[href^="reply?"]').click(function(e) {
-	e.preventDefault();
-	$(this).hide();
-	link = 'http://news.ycombinator.com/' + $(this).attr('href');
-	$(this).after(
-		'<div class="replyform" style="width:300px;height:100px;position:relative;"> \
-		<textarea rows="4" cols="60" style="height:60px;" /> \
-		<input type ="submit" value="Reply" class="rbutton" \
-		 style="position:absolute;bottom:0px;left:0px;" /> \
-		</div>'
-	);
-	$(this).parent().find('.rbutton').attr('data', link);
-});
+	init: function() {
+		$('a[href^="reply?"]').click(function(e) {
+			e.preventDefault();
+			$(this).hide();
+			link = 'http://news.ycombinator.com/' + $(this).attr('href');
+			$(this).after(
+				'<div class="replyform" style="width:300px;height:100px;position:relative;"> \
+				<textarea rows="4" cols="60" style="height:60px;" /> \
+				<input type ="submit" value="Reply" class="rbutton" \
+				 style="position:absolute;bottom:0px;left:0px;" /> \
+				</div>'
+			);
+			$(this).parent().find('.rbutton').attr('data', link);
+		});
 
-$('.rbutton').live('click', function(e) {
-	e.preventDefault();
-	link = $(this).attr('data');
-	text = $(this).prev().val();
-	postCommentTo(link, text, $(this));
-});
+		$('.rbutton').live('click', function(e) {
+			e.preventDefault();
+			link = $(this).attr('data');
+			text = $(this).prev().val();
+			InlineReply.postCommentTo(link, text, $(this));
+		});
+	},
 
-function postCommentTo(link, text, button) {
-	disableButtonAndBox(button);
-	$.ajax({
-		accepts: "text/html",
-		url : link
-	}).success(function(html) {
-		input = $(html).find('input');
-		fnid = input.attr('value');
-		sendComment(fnid, text);
-	}).error(function(xhr, status, error) {
-		enableButtonAndBox(button);
-	});
+	postCommentTo: function(link, text, button) {
+		InlineReply.disableButtonAndBox(button);
+		$.ajax({
+			accepts: "text/html",
+			url : link
+		}).success(function(html) {
+			input = $(html).find('input');
+			fnid = input.attr('value');
+			InlineReply.sendComment(fnid, text);
+		}).error(function(xhr, status, error) {
+			InlineReply.enableButtonAndBox(button);
+		});
+	},
+
+	sendComment: function(fnidarg, textarg) {
+		$.post(
+			"http://news.ycombinator.com/r", 
+			{fnid : fnidarg, text: textarg }
+		).complete(function(a) {
+			window.location.reload(true);
+		});	
+	},
+
+	disableButtonAndBox: function(button) {
+		button.attr('disabled','disabled');
+		button.prev().attr('disabled', 'disabled');
+	},
+
+	enableButtonAndBox: function(button) {
+		button.removeAttr('disabled');
+		button.prev().removeAttr('disabled');
+	}
 }
 
-function disableButtonAndBox(button) {
-	button.attr('disabled','disabled');
-	button.prev().attr('disabled', 'disabled');
-}
-
-function enableButtonAndBox(button) {
-	button.removeAttr('disabled');
-	button.prev().removeAttr('disabled');
-}
-
-function sendComment(fnidarg, textarg) {
-	$.post(
-		postLoc, 
-		{fnid : fnidarg, text: textarg }
-	).complete(function(a) {
-		window.location.reload(true);
-	});
-}
+InlineReply.init();
